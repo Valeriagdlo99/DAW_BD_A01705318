2ee97fa140b12ab1a149de7b597c72b158532f03
valeriagdlo99 <58562805+valeriagdlo@users.noreply.github.com>
2020-09-10 20:09:08 -0500

    Lab 10 creación de archivos y html

---
+++ Lab10/register.php
@@ -0,0 +1,7 @@
+<?php
+if ( !(isset($_POST["nombre"]) && isset($_POST["apellido"]) && isset($_POST["email"]) && isset($_POST["ingreso"]) && isset($_POST["monto"]) && isset($_POST["tipo"]))) {
+    die("Favor de poner los datos faltantes");
+  }
+
+
+?>
