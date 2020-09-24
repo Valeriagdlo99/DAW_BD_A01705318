1371eb2d24e816ea96c165173fbfc2aa63029e09
valeriagdlo99 <58562805+valeriagdlo@users.noreply.github.com>
2020-09-24 00:07:05 -0500

    Cargar imagen de perfil

--- Lab12/index.php
+++ Lab12/index.php
@@ -1,4 +1,12 @@
 <?php
+
+if(isset($_POST["nombre"])) {
+    $_SESSION["usuario"] = $_POST["usuario"];
+}
+
+if(!isset($_SESSION["usuario"])) {
+    $_SESSION["usuario"] = "No hay usuarios previos, ¡registrate!";
+}
 include("_head.html");
 include("_form.html");
 include("_preguntas.html");
